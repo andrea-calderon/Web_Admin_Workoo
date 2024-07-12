8e504656eb35b10026d198f3c57a9f14a85b9506
Andrea Alvanés <alvanesc@gmail.com>
2024-07-12 17:51:13 -0600

    feat(KAN-14): index files added to export the components centrally from Dashboard files

---
+++ src/features/dashboard/components/atoms/index.tsx
@@ -0,0 +1,3 @@
+// export {default as DashboardButton} from './DashboardButton'
+// export {default as DashboardInputField} from './DashboardInputField'
+// export {default as SimpleChart} from './SimpleChart'
