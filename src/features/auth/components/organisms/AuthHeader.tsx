935424764cca9b34ee83a3c2e5f6784f3ba3c340
Andrea Alvanés <alvanesc@gmail.com>
2024-08-22 20:35:40 -0600

    feat(KAN-32): Modified the Header and the Sidebar for basic Navigation. Added styles and tried to match the design

--- src/features/auth/components/organisms/AuthHeader.tsx
+++ src/features/auth/components/organisms/AuthHeader.tsx
@@ -1,3 +1,4 @@
+
 import NotificationsIcon from '@mui/icons-material/Notifications';
 import SearchIcon from '@mui/icons-material/Search';
 import SettingsIcon from '@mui/icons-material/Settings';
@@ -9,30 +10,66 @@ const AuthHeader = () => {
       position="fixed"
       sx={{
         backgroundColor: '#ffffff',
-        color: '#ffffff',
-        width: 'calc(100% - 240px)', 
-        left: '240px',              
-        zIndex: theme => theme.zIndex.drawer + 1, 
+        width: 'calc(100% - 240px)',
+        left: '240px',
+        zIndex: theme => theme.zIndex.drawer + 1,
+        boxShadow: 'none',
+        borderBottom: '1.5px solid #e6eff5',
       }}
     >
       <Toolbar sx={{ justifyContent: 'space-between', padding: '0 16px' }}>
-        <Typography variant="h6" sx={{ flexGrow: 1, textAlign: 'left', color: '#343c6a' }}>
+        <Typography
+          variant="h6"
+          sx={{ flexGrow: 1, textAlign: 'left', color: '#343c6a' }}
+        >
           Overview
         </Typography>
         <Box sx={{ display: 'flex', alignItems: 'center' }}>
-          <Box sx={{ display: 'flex', alignItems: 'center', ml: 2, backgroundColor: '#ffffff', borderRadius: 1, pl: 1 }}>
-          <SearchIcon sx={{ color: '#718ebf' }} />
-          <InputBase placeholder="Search for something" sx={{ ml: 1, flex: 1, color: '#718ebf' }} />
+          <Box
+            sx={{
+              display: 'flex',
+              alignItems: 'center',
+              backgroundColor: '#f5f7fa', 
+              borderRadius: '20px', 
+              p: '1.5',
+              boxShadow: '0px 4px 6px rgba(0, 0, 0, 0.1)',
+            }}
+          >
+            <SearchIcon sx={{ color: '#718ebf', ml: 2.5 }} />
+            <InputBase
+              placeholder="Search for something"
+              sx={{
+                flex: 1,
+                color: '#343c6a',
+              }}
+            />
           </Box>
         </Box>
-        <Box sx={{ display: 'flex', alignItems: 'center' }}>
-          <IconButton sx={{ color: '#718ebf' }}>
+        <Box sx={{ display: 'flex', alignItems: 'center', ml: 3 }}>
+          <IconButton
+            sx={{
+              backgroundColor: '#f5f7fa', 
+              borderRadius: '50%', 
+              p: 1.5,
+              boxShadow: '0px 4px 6px rgba(0, 0, 0, 0.1)', 
+              color: '#343c6a',
+              mr: 2,
+            }}
+          >
             <SettingsIcon />
           </IconButton>
-          <IconButton sx={{ color: '#fe5c73' }}>
+          <IconButton
+            sx={{
+              backgroundColor: '#f5f7fa', 
+              borderRadius: '50%', 
+              p: 1.5, 
+              color: '#fe5c73',
+             boxShadow: '0px 4px 6px rgba(0, 0, 0, 0.1)',
+            }}
+          >
             <NotificationsIcon />
           </IconButton>
-          <Avatar alt="User Profile" src="/path-to-image.jpg" sx={{ ml: 2 }} />
+          <Avatar alt="User Profile" src="/path-to-image.jpg" sx={{ ml: 3 }} />
         </Box>
       </Toolbar>
     </AppBar>
