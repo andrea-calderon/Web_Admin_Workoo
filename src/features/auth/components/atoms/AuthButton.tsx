0de34ee22289f24f0cd9fb32809d94ef93de085d
Andrea Alvanés <alvanesc@gmail.com>
2024-09-04 22:04:19 -0600

    feat(KAN-40): Create the component of button with main variants

--- src/features/auth/components/atoms/AuthButton.tsx
+++ src/features/auth/components/atoms/AuthButton.tsx
@@ -1,17 +1,56 @@
-import React from 'react';
 import Button from '@mui/material/Button';
-import { SxProps, Theme } from '@mui/material/styles';
+import { SxProps, Theme, useTheme } from '@mui/material/styles';
+import React from 'react';
 
 interface AuthButtonProps {
   onClick: () => void;
   children: React.ReactNode;
+  variant?: 'filled' | 'outlined' | 'text' | 'elevated' | 'tonal';
+  disabled?: boolean;
   sx?: SxProps<Theme>;
 }
 
-const AuthButton: React.FC<AuthButtonProps> = ({ onClick, children, sx }) => (
-  <Button variant="contained" color="primary" onClick={onClick} sx={sx}>
-    {children}
-  </Button>
-);
+const AuthButton: React.FC<AuthButtonProps> = ({ onClick, children, variant = 'filled', disabled = false, sx }) => {
+  const theme = useTheme();
+
+  const muiVariant = variant === 'filled' || variant === 'elevated' || variant === 'tonal' ? 'contained' : variant;
+
+  
+  const customStyles = {
+    
+    ...(disabled && {
+      backgroundColor: theme.palette.action.disabledBackground, 
+      color: theme.palette.action.disabled, 
+      cursor: 'not-allowed',
+    }),
+  };
+
+
+  return (
+    <Button
+      variant={muiVariant}
+      color="primary"
+      onClick={onClick}
+      sx={{
+        borderRadius: '100px', 
+        ...customStyles,
+        backgroundColor: variant === 'tonal' 
+          ? theme.palette.secondary.light 
+          : variant === 'elevated'
+          ? theme.palette.primary.light
+          :undefined,
+        color: variant === 'tonal' 
+        ? theme.palette.text.primary 
+        : variant === 'elevated'
+        ? theme.palette.primary.main
+        :undefined,
+        
+        ...sx, 
+      }}
+    >
+      {children}
+    </Button>
+  );
+};
 
 export default AuthButton;
