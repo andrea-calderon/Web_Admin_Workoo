ac298abfa657cc58dea79fc07b6fb4110736b23d
Andrea Alvanés <alvanesc@gmail.com>
2024-09-12 22:08:41 -0600

    feat(KAN-49): Create and implement of the TextAtom component and its variants.

---
+++ src/styles/themes/textTheme.ts
@@ -0,0 +1,48 @@
+import { TypographyOptions } from '@mui/material/styles/createTypography';
+
+
+export const textTheme: TypographyOptions = {
+  displayLarge: {
+    fontFamily: 'Roboto',
+    fontWeight: 400,
+    fontSize: '57px',
+    lineHeight: '64px',
+    letterSpacing: '0px',
+  },
+  displayMedium: {
+    fontFamily: 'Roboto',
+    fontWeight: 400,
+    fontSize: '45px',
+    lineHeight: '52px',
+    letterSpacing: '0px',
+  },
+  displaySmall: {
+    fontFamily: 'Roboto',
+    fontWeight: 400,
+    fontSize: '36px',
+    lineHeight: '44px',
+    letterSpacing: '0px',
+  },
+  headlineLarge: {
+    fontFamily: 'Roboto',
+    fontWeight: 400,
+    fontSize: '32px',
+    lineHeight: '40px',
+    letterSpacing: '0px',
+  },
+  headlineMedium: {
+    fontFamily: 'Roboto',
+    fontWeight: 400,
+    fontSize: '28px',
+    lineHeight: '36px',
+    letterSpacing: '0px',
+  },
+  headlineSmall: {
+    fontFamily: 'Roboto',
+    fontWeight: 400,
+    fontSize: '24px',
+    lineHeight: '32px',
+    letterSpacing: '0px',
+  },
+  
+};
