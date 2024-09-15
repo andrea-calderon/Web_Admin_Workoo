155621de583d7f77979195dccaf8186122815e85
Andrea Alvanés <alvanesc@gmail.com>
2024-09-15 11:25:50 -0600

    feat(KAN-67): AuthenticatedApp route protecction implementation for LogIn only.

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -1,16 +1,47 @@
 import AuthenticatedApp from '@/features/auth/components/pages/AuthenticatedApp';
 import LoginPage from '@/features/auth/components/pages/LoginPage';
 import RegisterPage from '@/features/auth/components/pages/RegisterPage';
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import { useState } from 'react';
+import {
+  Navigate,
+  Route,
+  BrowserRouter as Router,
+  Routes,
+} from 'react-router-dom';
 
-const AppRouter = () => (
-  <Router>
-    <Routes>
-      <Route path="/" element={<LoginPage />} />
-      <Route path="/register" element={<RegisterPage />} />
-      <Route path="/app/*" element={<AuthenticatedApp />} />
-    </Routes>
-  </Router>
-);
+const PrivateRoute = ({
+  isAuthenticated,
+  children,
+}: {
+  isAuthenticated: boolean;
+  children: JSX.Element;
+}) => {
+  return isAuthenticated ? children : <Navigate to="/" />;
+};
+
+const AppRouter = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
+
+  const handleLogin = () => {
+    setIsAuthenticated(true);
+  };
+
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<LoginPage onLogin={handleLogin} />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route
+          path="/app/*"
+          element={
+            <PrivateRoute isAuthenticated={isAuthenticated}>
+              <AuthenticatedApp />
+            </PrivateRoute>
+          }
+        />
+      </Routes>
+    </Router>
+  );
+};
 
 export default AppRouter;
