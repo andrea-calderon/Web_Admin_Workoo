c1df3a3d72a4859b5eeb61fd38f556770b27457b
Andrea Alvanés <alvanesc@gmail.com>
2024-09-18 21:53:23 -0600

    feat(KAN-69): Create the Initial screen component with the principal image.

---
+++ src/features/auth/components/pages/InitialPage.tsx
@@ -0,0 +1,88 @@
+import TextAtom from '@/features/components/TextAtom';
+import { Box, Grid } from '@mui/material';
+import { useTranslation } from 'react-i18next';
+import { useLocation } from 'react-router-dom';
+import Login from '../organisms/Login';
+import Signup from '../organisms/Signup';
+
+interface InitialPageProps {
+  onLogin: () => void;
+}
+
+const InitialPage: React.FC<InitialPageProps> = ({ onLogin }) => {
+  const location = useLocation();
+  const { t } = useTranslation();
+
+  return (
+    <Grid container sx={{ height: '100vh' }}>
+      <Grid
+        item
+        xs={12}
+        md={6}
+        sx={{
+          backgroundColor: '#F4F4F4',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          flexDirection: 'column',
+          padding: '20px',
+        }}
+      >
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'center',
+            width: '100%',
+            maxWidth: '500px',
+            mx: 'auto',
+            textAlign: 'center',
+          }}
+        >
+          <TextAtom
+            variant="headline"
+            size="small"
+            sx={{
+              mb: 2,
+              wordBreak: 'break-word',
+              overflowWrap: 'break-word',
+              letterSpacing: '0.5px',
+              fontWeight: 'bold',
+            }}
+          >
+            {t('initialScreen.initialTitle')}
+          </TextAtom>
+        </Box>
+        <Box sx={{ textAlign: 'center' }}>
+          <img
+            src="src\assets\images\Main_screen_img_bg.svg"
+            alt="Initial screen"
+            style={{
+              width: '400px',
+              borderRadius: '5%',
+            }}
+          />
+        </Box>
+      </Grid>
+
+      <Grid
+        item
+        xs={12}
+        md={6}
+        sx={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          padding: '40px',
+          backgroundColor: '#fff',
+        }}
+      >
+        {location.pathname === '/login' && <Login onLogin={onLogin} />}
+        {location.pathname === '/signup' && <Signup />}
+      </Grid>
+    </Grid>
+  );
+};
+
+export default InitialPage;
